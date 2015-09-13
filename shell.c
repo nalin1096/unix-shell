6011905cc051771b0ec60bb589370ff0d6992fe3
nalin14065 <nalin1096@hotmail.com>
2015-09-14 00:26:00 +0530

    piping and redirection

--- shell.c
+++ shell.c
@@ -12,23 +12,20 @@
 #define buffer_size 1024
 #define token_size 64
 #define token_delim " \n\t\r\a"
-#define IN 1
-#define OUT 2
 
 int shelly_cd (char **args);
 int shelly_help (char **args);
 int shelly_exit (char **args);
 int shelly_history (char **args);
-int shelly_easter_eggs (char **args);
+int shelly_hi (char **args);
 
-int redirect;
 int his_pos;
 
 char cmd_history[500][100];
 
-char *builtin_str[] = {"cd","help","history","exit"};
+char *builtin_str[] = {"cd","help","history","exit","hi"};
 
-int (*builtin_func[]) (char**) = {&shelly_cd,&shelly_help,&shelly_history,&shelly_exit};
+int (*builtin_func[]) (char**) = {&shelly_cd,&shelly_help,&shelly_history,&shelly_exit,&shelly_hi};
 
 int num_builtins () {
 	return sizeof(builtin_str) / sizeof(char*);
@@ -71,15 +68,34 @@ int shelly_history (char **args) {
 	return 1;
 }
 
+int shelly_hi (char **args) {
+	char *arg [] = {"say","Hi, my name is shelly, I was developed by Nalin Gupta.",NULL};
+	pid_t pid, wpid;
+	int status;
+
+	pid = fork();
+	if (pid==0) {
+		if (execvp(arg[0],arg)==-1) {
+			perror("shelly");
+			printf("\n");
+		}
+		return (EXIT_FAILURE);
+	}
+	else if (pid < 0 ) {
+		perror("shelly");
+		printf("\n");
+	}
+	else {
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return 1;
+}
+
 void add_history (char *line) {
 	strcpy(cmd_history[his_pos], line);
 	his_pos++;
-}
-
-void signal_handler (int sig) {
-	signal(SIGINT,signal_handler);
-	printf("\nSHELLY> ");
-	fflush(stdout);
 }
 
 char *get_username () {
@@ -98,6 +114,12 @@ char *get_username () {
 	exit (EXIT_FAILURE);
 }
 
+void signal_handler (int sig) {
+	signal(SIGINT,signal_handler);
+	printf("\nshelly: %s$ ", get_username());
+	fflush(stdout);
+}
+
 char *shelly_read_line () {
 	char *line = NULL;
 	ssize_t buffer = 0;
@@ -110,8 +132,6 @@ char **shelly_split_line (char *line) {
 	int pos =0;
 	char **tokens = malloc(buffer * sizeof(char*));
 	char *token;
-
-	redirect = 0;
 
 	if (!tokens) {
 		fprintf(stderr, "shelly: Allocation error\n");
@@ -162,9 +182,180 @@ int shelly_launch (char **args) {
 	return 1;
 }
 
+int redirect_out (char **args, int t) {
+	pid_t pid, wpid;
+	int status;
+	int out = open (args[t], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+	int stdoutcopy = dup(1);
+	dup2 (out,1);
+	close(out);
+
+
+	pid = fork();
+	if (pid==0) {
+		if (execvp(args[0],args)==-1) {
+			perror("shelly");
+			printf("\n");
+			dup2(stdoutcopy,1);
+			close(stdoutcopy);
+		}
+		return (EXIT_FAILURE);
+	}
+	else if (pid < 0 ) {
+		perror("shelly");
+		printf("\n");
+		dup2(stdoutcopy,1);
+	}
+	else {
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	dup2(stdoutcopy,1);
+	close(stdoutcopy);
+	return 1;
+}
+
+int redirect_in (char **args, int t) {
+	pid_t pid, wpid;
+	int status;
+	int in = open(args[t], O_RDONLY);
+	int stdincopy = dup(0);
+	dup2 (in,0);
+	close(in);
+
+	pid = fork();
+	if (pid==0) {
+		if (execvp(args[0],args)==-1) {
+			perror("shelly");
+			printf("\n");
+			dup2(stdincopy,0);
+		}
+		return (EXIT_FAILURE);
+	}
+	else if (pid < 0 ) {
+		perror("shelly");
+		printf("\n");
+		dup2(stdincopy,0);
+	}
+	else {
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	dup2(stdincopy,0);
+	return 1;
+}
+
+int piping(char **args, int pipes) {
+    const int commands = pipes + 1;
+    int i = 0;
+
+    int pipefds[2*pipes];
+
+    for(i = 0; i < pipes; i++){
+        if(pipe(pipefds + i*2) < 0) {
+            perror("Couldn't Pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    int pid;
+    int status;
+
+    int j = 0;
+    int k = 0;
+    int s = 1;
+    int place;
+    int commandStarts[10];
+    commandStarts[0] = 0;
+
+    while (args[k] != NULL){
+        if(!strcmp(args[k], "|")){
+            args[k] = NULL;
+            commandStarts[s] = k+1;
+            s++;
+        }
+        k++;
+    }
+
+
+
+    for (i = 0; i < commands; ++i) {
+        place = commandStarts[i];
+
+        pid = fork();
+        if(pid == 0) {
+            if(i < pipes){
+                if(dup2(pipefds[j + 1], 1) < 0){
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            if(j != 0 ){
+                if(dup2(pipefds[j-2], 0) < 0){
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            int q;
+            for(q = 0; q < 2*pipes; q++){
+                    close(pipefds[q]);
+            }
+
+            if( execvp(args[place], args+place) < 0 ){
+                    perror(*args);
+                    exit(EXIT_FAILURE);
+            }
+        }
+        else if(pid < 0){
+            perror("error");
+            exit(EXIT_FAILURE);
+        }
+
+        j+=2;
+    }
+
+    for(i = 0; i < 2 * pipes; i++){
+        close(pipefds[i]);
+    }
+
+    for(i = 0; i < pipes + 1; i++){
+        wait(&status);
+    }
+    return 1;
+}
+
 int shelly_execute (char **args) {
+	int pipes=0;
+
 	if (args[0] == NULL) {
 		return 1;
+	}
+
+	for (int i=0;args[i]!=NULL;i++) {
+		if (strcmp(args[i],">")==0) {
+			return redirect_out(args, i+1);
+		}
+	}
+
+	for (int i=0;args[i]!=NULL;i++) {
+		if (strcmp(args[i],"<")==0) {
+			return redirect_in(args, i+1);
+		}
+	}
+
+
+	for (int i=0;args[i]!=NULL;i++) {
+		if (strcmp(args[i],"|")==0) {
+			pipes++;
+		}
+	}
+
+	if (pipes > 0) {
+		return piping(args,pipes);
 	}
 
 	for (int i=0;i<num_builtins();i++) {
